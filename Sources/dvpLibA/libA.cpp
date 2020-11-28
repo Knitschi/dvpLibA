58551b05037f901e930caaf9c12ab68fc45f29f1
Knitschi <knitschi@freenet.de>
2020-11-28 17:36:29 +0100

    use macros for namespace

--- Sources/dvpLibA/libA.cpp
+++ Sources/dvpLibA/libA.cpp
@@ -7,12 +7,14 @@
 #define dvpLibB2Ver dvpLibB2_1_0_0
 #include CPF_VERSIONED_INCLUDE(dvpLibB2Ver, libB2.h)
 
+#define dvpLibCVer_from_dvpLibB2 dvpLibC_1_1_0
+#define dvpLibCVer_from_dvpLibB1 dvpLibC_2_0_0
 
 namespace dvpLibA
 {
-	dvpLibC_1_1_0::ValueType to_1_1_0ValueType(dvpLibC_2_0_0::ValueType value)
+	dvpLibCVer_from_dvpLibB2::ValueType to_1_1_0ValueType(dvpLibCVer_from_dvpLibB1::ValueType value)
 	{
-		dvpLibC_1_1_0::ValueType oldVersion;
+		dvpLibCVer_from_dvpLibB2::ValueType oldVersion;
 
 		oldVersion.foo = value.foo;
 		oldVersion.bar = value.bar;
@@ -20,9 +22,9 @@ namespace dvpLibA
 		return oldVersion;
 	}
 
-	dvpLibC_2_0_0::ValueType to_2_0_0ValueType(dvpLibC_1_1_0::ValueType value)
+	dvpLibCVer_from_dvpLibB1::ValueType to_2_0_0ValueType(dvpLibCVer_from_dvpLibB2::ValueType value)
 	{
-		dvpLibC_2_0_0::ValueType newVersion;
+		dvpLibCVer_from_dvpLibB1::ValueType newVersion;
 
 		newVersion.foo = value.foo;
 		newVersion.bar = value.bar;
@@ -32,50 +34,50 @@ namespace dvpLibA
 
 	void workWithValueType()
 	{
-		auto foo1 = dvpLibB1_2_0_0::b1ReturnValueType();
-		dvpLibB2_1_0_0::b2UseValueType(to_1_1_0ValueType(foo1));
+		auto foo1 = dvpLibB1Ver::b1ReturnValueType();
+		dvpLibB2Ver::b2UseValueType(to_1_1_0ValueType(foo1));
 
-		auto foo2 = dvpLibB2_1_0_0::b2ReturnValueType();
-		dvpLibB1_2_0_0::b1UseValueType(to_2_0_0ValueType(foo2));
+		auto foo2 = dvpLibB2Ver::b2ReturnValueType();
+		dvpLibB1Ver::b1UseValueType(to_2_0_0ValueType(foo2));
 	}
 
-	dvpLibC_1_1_0::ComplexType* to_1_1_0ComplexType(dvpLibC_2_0_0::ComplexType* ct)
+	dvpLibCVer_from_dvpLibB2::ComplexType* to_1_1_0ComplexType(dvpLibCVer_from_dvpLibB1::ComplexType* ct)
 	{
-		return reinterpret_cast<dvpLibC_1_1_0::ComplexType*>(ct);
+		return reinterpret_cast<dvpLibCVer_from_dvpLibB2::ComplexType*>(ct);
 	}
 
-	dvpLibC_2_0_0::ComplexType* to_2_0_0ComplexType(dvpLibC_1_1_0::ComplexType* ct)
+	dvpLibCVer_from_dvpLibB1::ComplexType* to_2_0_0ComplexType(dvpLibCVer_from_dvpLibB2::ComplexType* ct)
 	{
-		return reinterpret_cast<dvpLibC_2_0_0::ComplexType*>(ct);
+		return reinterpret_cast<dvpLibCVer_from_dvpLibB1::ComplexType*>(ct);
 	}
 
-	std::unique_ptr<dvpLibC_1_1_0::ComplexType> to_1_1_0UniqueComplexType(std::unique_ptr<dvpLibC_2_0_0::ComplexType> ct)
+	std::unique_ptr<dvpLibCVer_from_dvpLibB2::ComplexType> to_1_1_0UniqueComplexType(std::unique_ptr<dvpLibCVer_from_dvpLibB1::ComplexType> ct)
 	{
-		return std::unique_ptr<dvpLibC_1_1_0::ComplexType>(to_1_1_0ComplexType(ct.release()));
+		return std::unique_ptr<dvpLibCVer_from_dvpLibB2::ComplexType>(to_1_1_0ComplexType(ct.release()));
 	}
 
-	std::unique_ptr<dvpLibC_2_0_0::ComplexType> to_2_0_0UniqueComplexType(std::unique_ptr<dvpLibC_1_1_0::ComplexType> ct)
+	std::unique_ptr<dvpLibCVer_from_dvpLibB1::ComplexType> to_2_0_0UniqueComplexType(std::unique_ptr<dvpLibCVer_from_dvpLibB2::ComplexType> ct)
 	{
-		return std::unique_ptr<dvpLibC_2_0_0::ComplexType>(to_2_0_0ComplexType(ct.release()));
+		return std::unique_ptr<dvpLibCVer_from_dvpLibB1::ComplexType>(to_2_0_0ComplexType(ct.release()));
 	}
 
 	void workWithComplexType()
 	{
-		auto foo1 = dvpLibB1_2_0_0::b1ReturnComplexType();
-		dvpLibB2_1_0_0::b2UseComplexType(to_1_1_0ComplexType(foo1));
+		auto foo1 = dvpLibB1Ver::b1ReturnComplexType();
+		dvpLibB2Ver::b2UseComplexType(to_1_1_0ComplexType(foo1));
 
-		auto foo2 = dvpLibB2_1_0_0::b2ReturnComplexType();
-		dvpLibB1_2_0_0::b1UseComplexType(to_2_0_0ComplexType(foo2));
+		auto foo2 = dvpLibB2Ver::b2ReturnComplexType();
+		dvpLibB1Ver::b1UseComplexType(to_2_0_0ComplexType(foo2));
 
-		auto foo3 = dvpLibB1_2_0_0::b1CreateComplexType();
-		dvpLibB2_1_0_0::b2SetComplexType(to_1_1_0UniqueComplexType(std::move(foo3)));
+		auto foo3 = dvpLibB1Ver::b1CreateComplexType();
+		dvpLibB2Ver::b2SetComplexType(to_1_1_0UniqueComplexType(std::move(foo3)));
 
-		auto foo4 = dvpLibB2_1_0_0::b2CreateComplexType();
-		dvpLibB1_2_0_0::b1SetComplexType(to_2_0_0UniqueComplexType(std::move(foo4)));
+		auto foo4 = dvpLibB2Ver::b2CreateComplexType();
+		dvpLibB1Ver::b1SetComplexType(to_2_0_0UniqueComplexType(std::move(foo4)));
 
-		auto foo5 = std::make_unique<dvpLibC_1_1_0::ComplexType>();
-		dvpLibB2_1_0_0::b2UseComplexType(foo5.get());
-		dvpLibB1_2_0_0::b1UseComplexType(to_2_0_0ComplexType(foo5.get()));
+		auto foo5 = std::make_unique<dvpLibCVer_from_dvpLibB2::ComplexType>();
+		dvpLibB2Ver::b2UseComplexType(foo5.get());
+		dvpLibB1Ver::b1UseComplexType(to_2_0_0ComplexType(foo5.get()));
 	}
 
 }
