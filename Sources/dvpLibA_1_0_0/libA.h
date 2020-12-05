0b9c691785c02a104d9e2fd63815e5981b9f5dfa
Knitschi <knitschi@freenet.de>
2020-12-05 15:59:52 +0100

    Switch to using an unversioned package namespace name to remove the version from the export macros and the export macro header filename.

--- Sources/dvpLibA_1_0_0/libA.h
+++ Sources/dvpLibA_1_0_0/libA.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "dvpLibAVersionMacros.h"
-#include CPF_VERSIONED_INCLUDE(dvpLibA, dvpLibA_1_0_0_export.h)
+#include CPF_VERSIONED_INCLUDE(dvpLibA, dvpLibA_export.h)
 
 namespace dvpLibA
 {
-	DVPLIBA_1_0_0_EXPORT void workWithValueType();
-	DVPLIBA_1_0_0_EXPORT void workWithComplexType();
+	DVPLIBA_EXPORT void workWithValueType();
+	DVPLIBA_EXPORT void workWithComplexType();
 }
 
