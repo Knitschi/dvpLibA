a04e11e5a26b504807777f8effc6dc7da4d43c71
Knitschi <knitschi@freenet.de>
2020-12-21 19:09:49 +0100

    - Adds a private dependency - Adds dvpLibD in a different version. - updates packages.

--- Sources/packages.cmake
+++ Sources/packages.cmake
@@ -2,6 +2,7 @@ set( CPF_PACKAGES
 	EXTERNAL CPFCMake
     EXTERNAL CPFBuildscripts
     EXTERNAL dvpLibD_1_0_0
+    EXTERNAL dvpLibD_1_1_0
     EXTERNAL dvpLibC_2_4_0
     EXTERNAL dvpLibC_2_5_0
     EXTERNAL dvpLibB1_2_0_0
