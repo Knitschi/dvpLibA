4ed8e2140b57d96e254e41545cdd19127eb30045
Knitschi <knitschi@freenet.de>
2020-12-23 13:42:34 +0100

    - Updates dvpLibD. - Updates dvpLibC.

--- Sources/packages.cmake
+++ Sources/packages.cmake
@@ -1,9 +1,9 @@
 set( CPF_PACKAGES 
 	EXTERNAL CPFCMake
     EXTERNAL CPFBuildscripts
-    EXTERNAL dvpLibD_1_0_0
+    EXTERNAL dvpLibD_1_2_0
     EXTERNAL dvpLibD_1_1_0
-    EXTERNAL dvpLibC_2_4_0
+    EXTERNAL dvpLibC_2_6_0
     EXTERNAL dvpLibC_2_5_0
     EXTERNAL dvpLibB1_2_0_0
     EXTERNAL dvpLibB2_1_0_0
